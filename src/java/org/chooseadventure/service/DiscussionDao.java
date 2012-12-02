9df8b977262f1150b997705d28e13a81f3df4951
Kevin Gomes <kevingomes17@gmail.com>
2012-12-02 00:05:23 -0600

    Merging code conflicts

--- src/java/org/chooseadventure/service/DiscussionDao.java
+++ src/java/org/chooseadventure/service/DiscussionDao.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -95,7 +94,7 @@ public class DiscussionDao extends BaseDao {
 
             //get topic
             TypedQuery<Discussiontopic> dtopicQuery = em.createNamedQuery("Discussiontopic.findById", Discussiontopic.class);
-            dtopicQuery.setParameter("topicId", new BigDecimal(hm.get("topic").toString()));
+            dtopicQuery.setParameter("topicId", new BigDecimal(hm.get("topicId").toString()));
             Discussiontopic topic = dtopicQuery.getSingleResult();
 
             Userbase user = getUserObj(hm.get("user").toString());
@@ -119,90 +118,3 @@ public class DiscussionDao extends BaseDao {
 
     }
 }
-=======
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package org.chooseadventure.service;
-
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-//import javax.management.j2ee.statistics.JDBCConnectionPoolStats;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-import javax.persistence.TypedQuery;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import org.chooseadventure.entity.*;
-import java.util.HashMap;
-import javax.persistence.TemporalType;
-
-
-import org.springframework.stereotype.Service;
-
-/**
- *
- * @author root
- */
-@Service
-public class DiscussionDao extends BaseDao {
-
-    //get all Discussion topics for attraction
-    public List<Discussiontopic> getDiscussionTopicsbyAtt(String attractionId) {
-        try {
-            //get discussion topics by attraction
-            TypedQuery<Discussiontopicbyattraction> dTopicByAttQuery = em.createNamedQuery("Discussiontopicbyattraction.findByAttractionid", Discussiontopicbyattraction.class);
-            dTopicByAttQuery.setParameter("attractionid", new BigDecimal(attractionId));
-            List<Discussiontopicbyattraction> dTopicsByAtt = dTopicByAttQuery.getResultList();
-            
-            //get discussion topic in each Discussiontopicbyattraction and add to list
-            List<Discussiontopic> dTopic = new ArrayList< Discussiontopic>();
-
-            if (dTopicsByAtt.size() > 0) {
-                for (int i = 0; i < dTopicsByAtt.size(); i++) {
-                    dTopic.add(dTopicsByAtt.get(i).getDiscussiontopic());
-                }
-            }
-            //return list
-            return dTopic;    
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-    
-    public List<Discussionthread> getDiscussionThreadsFromTopic(String attractionId, String topicId){
-        try{
-            List<Discussiontopic> dTopic = getDiscussionTopicsbyAtt(attractionId);
-            List<Discussionthread> threads = (List)dTopic.get(Integer.parseInt(topicId)).getDiscussionthreadCollection();
-            return threads;
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        return null;
-    }
-    
-    public List<Discussionthreadcomment> getCommentsFromThread(String attractionId, String topicId, String threadId){
-        try{
-            List<Discussionthread> threads = getDiscussionThreadsFromTopic(attractionId, topicId);
-            
-            List<Discussionthreadcomment> comments = (List)threads.get(Integer.parseInt(threadId)).getDiscussionthreadcommentCollection();
-            return comments;
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        
-        
-        return null;
-    }
-    
-    //new thread
-    public void insertThread(){
-        
-    }
-}
->>>>>>> 53aa698cba07277e9c8f4fa23451cb968a3d0dd4
